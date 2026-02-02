eed143c90ca2098fba3afb8e2bcd9b6831bdc522
JKeiyuru <josephkeiyuru1@gmail.com>
2026-02-02 10:35:23 +0300

    Models made

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -8,6 +8,7 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  bom: any;
   constructor() {
     super({
       log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
